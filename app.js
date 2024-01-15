d0852291a990a6082b3cc75a3e7c7c791557e59b
NicolasCastela <nickcastela2022@hotmail.com>
2024-01-15 09:51:51 -0300

    add private router with token jwt

--- app.js
+++ app.js
@@ -17,6 +17,35 @@ app.get("/", (req, res) => {
   res.status(200).json({ msg: "Bem viundo a nossa API!" });
 });
 
+function checkToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split("")[1];
+  if (!token) {
+    return res.status(401).json({ msg: "Acesso negado" });
+  }
+  try {
+    const secret = process.env.SECRET;
+    jwt.verify(token, secret);
+
+    next();
+  } catch (e) {
+    return res.status(400).json({ msg: "token invalido" });
+  }
+}
+
+//Close Route
+app.get("/users/:id", checkToken, async (req, res) => {
+  const id = req.params.id;
+
+  //check if user exists
+
+  const user = await User.findById(id, "-password");
+  if (!user) {
+    return res.status(404).json({ msg: "usuario nao encontrado" });
+  }
+  return res.status(200).json(user);
+});
+
 //Register User
 app.post("/auth/register", async (req, res) => {
   const { name, email, password, confirmpassword } = req.body;
